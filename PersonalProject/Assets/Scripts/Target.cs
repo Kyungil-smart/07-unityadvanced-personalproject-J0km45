c0866f62716eae218c953cdb7137d098f8ff6d34
Jo Kyeongmin <skykm0405@naver.com>
2026-03-02 23:31:59 +0900

    타겟 스포너 추가

--- PersonalProject/Assets/Scripts/Target.cs
+++ PersonalProject/Assets/Scripts/Target.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using System;
 
 public class Target : MonoBehaviour, IHittable
 {
+    public Transform SpawnPoint { get; private set; }
+    public event Action<Target> OnDestroyed;
+
+    public void Init(Transform spawnPoint)
+    {
+        SpawnPoint = spawnPoint;
+    }
+
     public void OnHit()
     {
         // TODO: 점수 추가
+        OnDestroyed?.Invoke(this);
         Destroy(gameObject);
     }
 }
---
+++ PersonalProject/Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TargetSpawner : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _spawnPoints; // 스폰 위치
+    [SerializeField] private GameObject _targetPrefab; // 타겟
+    [SerializeField] private int _maxTargetCount = 10; // 최대 타겟 수
+    [SerializeField] private int _targetCount; // 생성된 타겟 수
+
+    private void Start()
+    {
+        SpawnTarget();
+    }
+
+    void SpawnTarget()
+    {
+        if (_targetPrefab == null) return;
+
+        while (_targetCount < _maxTargetCount)
+        {
+            int rand = Random.Range(0, _spawnPoints.Count);
+
+            Transform spawnPoint = _spawnPoints[rand];
+
+            GameObject t = Instantiate(_targetPrefab, spawnPoint.position, Quaternion.identity);
+            if (t.TryGetComponent(out Target target))
+            {
+                _spawnPoints.RemoveAt(rand);
+                target.Init(spawnPoint);
+                target.OnDestroyed += NotifyDestroy;
+                _targetCount++;
+            }
+        }
+    }
+
+    public void NotifyDestroy(Target target)
+    {
+        target.OnDestroyed -= NotifyDestroy;
+        _spawnPoints.Add(target.SpawnPoint);
+        _targetCount--;
+        SpawnTarget();
+    }
+}
